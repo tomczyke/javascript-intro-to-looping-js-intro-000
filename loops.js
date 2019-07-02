f97d0f5c95f619f0166012c7abbe9e64ddfa04e3
Evan Tomczyk <tomczykej@gmail.com>
2019-07-02 22:40:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ var array = new Array();
 
 function forLoop(array) {
   for (let i = 0; i < 2; i++) {
-    return 'I am 1 strange loop.'
+    array.push('I am 1 strange loop.')
   }
   for (let i = 1; i < 25; i++) {
     return 'I am ${i} strange loops.'
